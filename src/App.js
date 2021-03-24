64d330f841712c028e46e1e26e588b1d8addc14e
Gustavo Herrera <gustavoh.2312@gmail.com>
2021-03-24 11:05:34 -0500

    project viewer pdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import PDFViewer from 'pdf-viewer-reactjs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <PDFViewer
+          document={{
+            url: 'http://www.pdf995.com/samples/pdf.pdf',
+          }}
+        />
     </div>
   );
 }
